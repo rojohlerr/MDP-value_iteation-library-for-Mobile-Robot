b0e730135b1a7def167499596c48a30828b928e7
robel Hayelom <53154414+rojohlerr@users.noreply.github.com>
2020-07-25 06:02:03 +0200

    Create test.cpp
    the test for the library

---
+++ test.cpp
@@ -0,0 +1,19 @@
+sample code to run the program for test the algorithm 
+
+#include<iostream>
+#include"Mdplibrary.h"
+#include"Mdplibrary.cpp"
+using namespace std;
+
+int main()
+{
+    
+    Mdplibrary lib;
+    lib.Create_world();
+   
+    lib.caller();
+   
+ 
+    return 0;
+    
+}
